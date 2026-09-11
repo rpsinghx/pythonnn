7f0f6761a1021abe4bfff12ca6aa7acee37c2fdf
rpsinghx <rpsinghx9@gmail.com>
2026-09-11 11:09:46 +0530

    loops

---
+++ Loops.py
@@ -0,0 +1,22 @@
+#WHILE LOOP - continues until the statement is true
+
+#printing numbers from 1 to 5 using while loop 
+x=1 
+while x<=5:
+    print(x)
+    x+=1
+print("End")
+
+#printing numbers from 10 to 20 by using while loop
+y=10
+while (y>=10) and (y<=20):
+    print(y)
+    y+=2
+print("End")
+
+#for loop in python 
+item_costs=[10,20,30]
+sum=0
+for i in item_costs:
+    sum=sum+i
+print(sum)
